ce4a213063091a5de311c7c1ae782c68fba3ca40
Em Corkindale <emily.corkindale@gmail.com>
2023-10-07 16:52:09 +1300

    Exercise 6 - Using calc and logging library.

--- Controllers/calcController.js
+++ Controllers/calcController.js
@@ -1,36 +1,43 @@
+const Calculator = require("../Models/Calculator");
+let myCalc = new Calculator();
+const Logging = require("../Models/Logging")
+let log = new Logging();
+
 // Logic for addition
 const add = (req, res) => {
   let number1 = parseInt(req.query.num1);
   let number2 = parseInt(req.query.num2);
-  let sum = number1 + number2;
-  console.log(sum);
-  res.status(200);
-  res.json({ result: sum });
+  if (!isNaN(number1) && !isNaN(number2)) {
+    const result = myCalc.add(number1, number2);
+    log.log(myCalc.id, result)
+    res.json({ result });
+  }
 };
 // Logic for subtraction
 const subtract = (req, res) => {
   let number1 = parseInt(req.query.num1);
   let number2 = parseInt(req.query.num2);
-  let sum = number1 - number2;
-  console.log(sum);
-  res.status(200);
-  res.json({ result: sum });
+  if (!isNaN(number1) && !isNaN(number2)) {
+    const result = myCalc.subtract(number1, number2);
+    res.json({ result });
+  }
 };
 // Logic for division
 const divide = (req, res) => {
   let number1 = parseInt(req.query.num1);
   let number2 = parseInt(req.query.num2);
-  let sum = number1 / number2;
-  console.log(sum);
-  res.status(200);
-  res.json({ result: sum });
+  if (!isNaN(number1) && !isNaN(number2)) {
+    const result = myCalc.divide(number1, number2);
+    res.json({ result });
+  }
 };
 // Logic for multiplication
 const multiply = (req, res) => {
   let number1 = parseInt(req.query.num1);
   let number2 = parseInt(req.query.num2);
-  let sum = number1 * number2;
-  console.log(sum);
-  res.status(200);
-  res.json({ result: sum });
+  if (!isNaN(number1) && !isNaN(number2)) {
+    const result = myCalc.multiply(number1, number2);
+    res.json({ result });
+  }
 };
+module.exports = { add, subtract, divide, multiply };
---
+++ Models/Logging.js
@@ -0,0 +1,7 @@
+class Logging {
+    log(id, value){
+        console.log(`Calculator id is ${id} and the result is ${value}.`);
+    };
+    
+}
+module.exports = Logging
